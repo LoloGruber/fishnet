3ea375caa84160e5ce7dc5f4a9018c4cb7b35a81
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-06 18:09:15 +0200

    testing findneighbours

--- app/settlement_delineation_pattern_analysis/2_neighbours/FindNeighboursTask.cpp
+++ app/settlement_delineation_pattern_analysis/2_neighbours/FindNeighboursTask.cpp
@@ -1,7 +1,8 @@
-
+#include "FindNeighboursTask.h"
 
 int main(int argc, char const *argv[])
 {
-    
+    FindNeighboursTask<fishnet::geometry::Polygon<double>> task;
+    task.run();
     return 0;
 }
--- app/settlement_delineation_pattern_analysis/2_neighbours/FindNeighboursTask.h
+++ app/settlement_delineation_pattern_analysis/2_neighbours/FindNeighboursTask.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <vector>
+#include <fishnet/Shapefile.hpp>
+#include <fishnet/ShapeGeometry.hpp>
+#include <fishnet/VectorLayer.hpp>
+#include <fishnet/PolygonNearestNeighbours.hpp>
+#include "SettlementPolygon.hpp"
+
+
+template<fishnet::geometry::IPolygon P>
+class FindNeighboursTask{
+private:
+    std::vector<fishnet::Shapefile> inputs;
+
+public:
+
+    void run() {
+        std::vector<SettlementPolygon<P>> polygons;
+        for(const auto & shp : inputs) {
+            auto layer = fishnet::VectorLayer<P>::read(shp);
+            for(const auto & feature : layer.getFeatures()) {
+
+            }
+        }
+        auto result = findNeighbouringPolygons(polygons,fishnet::util::TrueBiPredicate());
+        
+    }
+};
