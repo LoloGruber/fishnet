e1d3c9c162f989e87d1e2ebb6308d16ca9eb7a36
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-06 18:07:34 +0200

    added wrapper for settlement polygon

---
+++ app/settlement_delineation_pattern_analysis/2_neighbours/SettlementPolygon.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <fishnet/ShapeGeometry.hpp>
+#include "MemgraphAdjacency.hpp"
+
+/**
+ * @brief Wrapper Object, which fulfills the requirements of a Polygon and for a Node stored in the Memgraph DB
+ * 
+ * @tparam P specific polygon type (e.g. Polygon<double>)
+ */
+template<fishnet::geometry::IPolygon P>
+class SettlementPolygon:public P{
+private:
+    size_t id; // unique id of the settlement
+    FileReference fileRef; // file, which stores the polygon
+public:
+    template<typename... Args>
+    SettlementPolygon(size_t id, FileReference fileRef, Args&&... args):P(std::forward<Args>(args)...),id(id),fileRef(std::move(fileRef)){}
+    size_t key() const noexcept {
+        return id;
+    }
+
+    const FileReference & file() const noexcept {
+        return fileRef;
+    }
+};
+
+static_assert(fishnet::geometry::IPolygon<SettlementPolygon<fishnet::geometry::Polygon<double>>>);
+static_assert(DatabaseNode<SettlementPolygon<fishnet::geometry::Polygon<double>>>);
