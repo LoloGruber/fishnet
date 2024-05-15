f9c9fe377b1e42b1cfc96d6b106818ed02d64d20
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-15 19:30:52 +0200

    settlement polygon is now more generic

--- app/settlement_delineation_pattern_analysis/shared/settlement/SettlementPolygon.hpp
+++ app/settlement_delineation_pattern_analysis/shared/settlement/SettlementPolygon.hpp
@@ -5,16 +5,16 @@
 /**
  * @brief Wrapper Object, which fulfills the requirements of a Polygon and for a Node stored in the Memgraph DB
  * 
- * @tparam P specific polygon type (e.g. Polygon<double>)
+ * @tparam S specific polygon type (e.g. Polygon<double>)
  */
-template<fishnet::geometry::IPolygon P>
-class SettlementPolygon:public P{
+template<fishnet::geometry::Shape S>
+class SettlementPolygon:public S{
 private:
     size_t id; // unique id of the settlement
     FileReference fileRef; // file, which stores the polygon
 public:
     template<typename... Args>
-    SettlementPolygon(size_t id, FileReference fileRef, Args&&... args):P(std::forward<Args>(args)...),id(id),fileRef(std::move(fileRef)){}
+    SettlementPolygon(size_t id, FileReference fileRef, Args&&... args):S(std::forward<Args>(args)...),id(id),fileRef(std::move(fileRef)){}
     size_t key() const noexcept {
         return id;
     }
@@ -23,7 +23,7 @@ public:
         return fileRef;
     }
 
-    bool operator==(const SettlementPolygon<P> & other) const noexcept {
+    bool operator==(const SettlementPolygon<S> & other) const noexcept {
         return this->key() == other.key();
     }
 };
