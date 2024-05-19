90a759caf2087a435066363efa9af5f7629f13d7
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-19 18:15:23 +0200

    added magic enum lib requirement

--- app/settlement_delineation_pattern_analysis/shared/filters/Filter.hpp
+++ app/settlement_delineation_pattern_analysis/shared/filters/Filter.hpp
@@ -17,27 +17,6 @@ enum class BinaryFilterType{
     InsidePolygonFilter
 };
 
-// constexpr static fishnet::util::StaticMap<const char *,UnaryFilterType> UNARY = {{
-//     {"ApproxAreaFilter",UnaryFilterType::ApproxAreaFilter},
-//     {"ProjectedAreaFilter",UnaryFilterType::ProjectedAreaFilter}
-// }};
-
-// using namespace std::literals::string_view_literals;
-// constexpr static std::array<std::pair<std::string_view,UnaryFilterType>,2> unary_data{{
-//     {"ApproxAreaFilter"sv,UnaryFilterType::ApproxAreaFilter},
-//     {"ProjectedAreaFilter"sv,UnaryFilterType::ProjectedAreaFilter}
-// }};
-
-// constexpr static std::array<std::pair<std::string_view,BinaryFilterType>,1> binary_data {{
-//     {"InsidePolygonFilter",BinaryFilterType::InsidePolygonFilter}
-// }};
-
-// constexpr static fishnet::util::StaticMap<std::string_view,UnaryFilterType,unary_data.size()> UNARY {unary_data};
-
-// constexpr static inline fishnet::util::StaticMap<std::string_view,BinaryFilterType,binary_data.size()> BINARY {binary_data};
-// }
-
-
 template<typename F>
 concept Filter = requires(const F & filter,const nlohmann::json & json) {
     {F::type()} -> std::same_as<UnaryFilterType>;
