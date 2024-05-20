e7bc3a5c68fcdac4fa8390ecf193a9c566929d8c
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-20 18:43:16 +0200

    removing testing fragments

--- app/settlement_delineation_pattern_analysis/3_contract/ContractionTask.cpp
+++ app/settlement_delineation_pattern_analysis/3_contract/ContractionTask.cpp
@@ -22,14 +22,6 @@ int main(int argc, const char * argv[]){
         }
     });
     app.add_option("-c,--config",configFilename,"Path to contract.json configuration")->required()->check(CLI::ExistingFile);
-    // app.add_option("-o,--output",outputStem,"Output file path, storing the merged polygons after performing the contraction on all inputs")->required()->check([](const std::string & str){
-    //     try{
-    //         auto asShp = fishnet::Shapefile(str);
-    //         return std::string();
-    //     }catch(std::invalid_argument & error){
-    //         return std::string("Output filename is not valid\n")+error.what();
-    //     }
-    // });
     app.add_option("--outputStem",outputStem,"Output file path, storing the merged polygons after performing the contraction on all inputs")->required();
     app.add_option("--outputDir",outputDirectory,"Output directory, created file at this directory with the filename ${outputStem}.shp")->required()->check(CLI::ExistingDirectory);
     CLI11_PARSE(app,argc,argv);
@@ -37,13 +29,11 @@ int main(int argc, const char * argv[]){
         throw std::runtime_error("No input files provided");
     ContractionConfig<GeometryType> config {json::parse(std::ifstream(configFilename))};
     auto outputPath = std::filesystem::path(outputDirectory) / std::filesystem::path(outputStem+".shp");
-    std::cout << outputPath <<std::endl;
     fishnet::Shapefile output {outputPath};
     ContractionTask<GeometryType> task {std::move(config),output};
     for(auto && input : inputFilenames) {
         task.addInput({input});
     }
-    config.contractBiPredicates.push_back(DistanceBiPredicate(500.0));
     config.workers = 1;
     task.run();
     return 0;
