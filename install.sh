f887bae0854b6af47eccb6512f3fb216cc1bce37
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-05-28 18:45:16 +0200

    do not build tests on install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
+cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=OFF -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 cmake --build . -j 8
 cmake --install .
