472fa8c8234c289d1704eaa490eb55beba525095
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-03 17:29:09 +0200

    relaxed constraints on graphs

--- lib/graph/model/include/fishnet/GraphModel.hpp
+++ lib/graph/model/include/fishnet/GraphModel.hpp
@@ -15,10 +15,10 @@ namespace fishnet::graph {
  */
 template<typename GraphImpl, typename E=GraphImpl::edge_type, typename N = E::node_type, typename A = GraphImpl::adj_container_type>
 concept Graph = Node<N> && Edge<E> && (std::derived_from<GraphImpl,AbstractGraph<GraphImpl,E,A>>) && requires(const GraphImpl & g, const N & n){
-    {g.getNodes()} -> util::view_of<const N>;
+    {g.getNodes()} -> util::forward_range_of<const N>;
     {g.getEdges()} -> util::forward_range_of<E>;
-    {g.getNeighbours(n)} -> util::view_of<const N>;
-    {g.getReachableFrom(n)} -> util::view_of<const N>;
+    {g.getNeighbours(n)} -> util::forward_range_of<const N>;
+    {g.getReachableFrom(n)} -> util::forward_range_of<const N>;
     {g.getInboundEdges(n)} -> util::forward_range_of<E>;
     {g.getOutboundEdges(n)} -> util::forward_range_of<E>;
     typename GraphImpl::node_type;
