731696ab0bedcb3ad44c132e0cbeba190dc42fdb
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-04 17:29:59 +0200

    decouple loading of memgraph project

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=OFF -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
-cmake --build . -j 16
+cmake --build . --config Release -j 16
 cmake --install .
+cd ..
+rm -rf build
