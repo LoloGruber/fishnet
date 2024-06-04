5a277767a8b9ebe09c1ca0c6177d3ddb91f211c3
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-04 17:29:18 +0200

    removed test folder from git

--- app/settlement_delineation_pattern_analysis/test/example/runExamples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf workingDir
-mkdir workingDir
-cd workingDir
-cwltool /home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cwl/filter.cwl ../filterExample.json
-cwltool /home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cwl/neighbours.cwl ../neighboursExample.json
-cwltool /home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cwl/contraction.cwl ../contractionExample.json
-cwltool /home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cwl/analysis.cwl ../analysisExample.json 
