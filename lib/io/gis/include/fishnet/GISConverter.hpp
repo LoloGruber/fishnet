f0778bd12b07526b5f78d1660b60672aae793cf2
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-05 18:10:50 +0200

    fixing field_name for pixel values set default for printing progress to false

--- lib/io/gis/include/fishnet/GISConverter.hpp
+++ lib/io/gis/include/fishnet/GISConverter.hpp
@@ -19,7 +19,7 @@ public:
      * @param showProgress show progress in console
      * @return std::expected<Shapefile,std::string>: Shapefile on success, otherwise string explaining the error
      */
-    static std::expected<Shapefile,std::string> convert(const GeoTiff & geoTiff,bool maskZero = true, bool showProgress=true) noexcept{
+    static std::expected<Shapefile,std::string> convert(const GeoTiff & geoTiff,bool maskZero = true, bool showProgress=false) noexcept{
         GDALAllRegister();
         GDALDataset *src = (GDALDataset *) GDALOpen(geoTiff.getPath().c_str(), GA_ReadOnly);
         if(src == nullptr)
@@ -31,7 +31,7 @@ public:
         GDALDataset *dest = driver->Create(destPath.c_str(), 0,0, 0, GDT_Unknown,nullptr);
         auto spatialReference = src->GetSpatialRef();
         OGRLayer * layer = dest->CreateLayer(destPath.stem().c_str(),spatialReference->Clone(),wkbPolygon, nullptr);
-        const char *fieldName = "pixel_value";
+        const char *fieldName = "pixel_val";
         OGRFieldDefn fieldDefn = OGRFieldDefn(fieldName, OFTInteger);
         if(layer->CreateField(&fieldDefn)!=OGRERR_NONE)
             return std::unexpected("Could not create field for pixel values");
