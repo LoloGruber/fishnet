362e97e9e8719ef993b04e455cf77940ca6e2eb1
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-05 15:30:41 +0200

    added command line arguments for workflow

--- app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.cpp
+++ app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.cpp
@@ -1,15 +1,20 @@
 #include "SettlementDelineation.h"
 #include <nlohmann/json.hpp>
+#include <CLI/CLI.hpp>
+#include <fishnet/GISFactory.hpp>
 
 
 
 int main(int argc, char * argv[]){
+    CLI::App app {"SettlementDelineation Workload-Generator and Scheduler"};
     std::string pathToCfg = "/home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cfg/workflow.json";
     std::string inputDirectory = "/home/lolo/Documents/fishnet/data/WSF/2019/Bolivia";
     std::vector<std::filesystem::path> inputFiles;
+    app.add_option("-c,--config",pathToCfg,"Path to workflow.json configuration")->required()->check(CLI::ExistingFile);
+    app.add_option("-i,--inputDirectory",inputDirectory,"Path to input directory")->required()->check(CLI::ExistingDirectory);
+    CLI11_PARSE(app,argc,argv);
     for(auto const& file : std::filesystem::directory_iterator(inputDirectory)){
-        auto ext = file.path().extension();
-        if(file.is_regular_file() && (ext==".shp" || ext==".tif")) {
+        if(file.is_regular_file() && fishnet::GISFactory::getType(file).has_value()) {
             inputFiles.push_back(file.path());
         }
     }
