c26f3cd86613924c396fb88b2b359911486ba9cd
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-07 19:58:02 +0200

    added fixed size priority queue

---
+++ lib/util/collections/include/fishnet/FixedSizePriorityQueue.hpp
@@ -0,0 +1,51 @@
+#pragma once 
+#include <queue>
+
+namespace fishnet::util {
+template<typename T, typename Mapper>
+class FixedSizePriorityQueue{
+private:
+    std::vector<T> queue;// Max-Heap queue
+    size_t capacity;
+    Mapper mapper;
+public:
+    FixedSizePriorityQueue(size_t capacity,Mapper comparator):capacity(capacity),mapper(comparator){
+        queue.reserve(capacity);
+    }
+
+    bool empty() const noexcept {
+        return queue.empty();
+    }
+
+    void push(T && value){
+        if(queue.size() < capacity){
+            queue.push_back(std::move(value));
+        }else{
+            int swapIndex=-1;
+            auto max = mapper(queue[0]);
+            for(int i=1; i < int(capacity);i++){
+                auto val = mapper(queue[i]);
+                if(val> max){
+                    max = val;
+                    swapIndex = i;
+                }
+            }
+            if(swapIndex!=-1 && mapper(value) < max)
+                queue[swapIndex] = std::move(value);
+        }
+    }
+
+    void push(const T & value){
+        T copy = value;
+        push(std::move(copy));
+    }
+
+    auto begin(){
+        return queue.begin();
+    }
+
+    auto end(){
+        return queue.end();
+    }
+};
+}
