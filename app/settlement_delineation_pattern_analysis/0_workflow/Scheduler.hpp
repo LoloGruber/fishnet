5a69b7505c749bf74de23400982a3abcac74e7a7
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-09 15:26:02 +0200

    fixed race conditions schedule until finished

--- app/settlement_delineation_pattern_analysis/0_workflow/Scheduler.hpp
+++ app/settlement_delineation_pattern_analysis/0_workflow/Scheduler.hpp
@@ -5,6 +5,7 @@
 
 class Scheduler {
 private:
+    mutable std::mutex mutex;
     JobDAG_t dag;
     Executor_t executor;
     JobType lastJobType;
@@ -18,6 +19,7 @@ private:
     }
 
     bool canBeScheduled( Job & job) const noexcept {
+        std::lock_guard lock {this->mutex};
         if(job.type > lastJobType){
             updateJobState(job,JobState::ABORTED);
             return false;
@@ -29,22 +31,49 @@ private:
 
     void updateJobState(Job & job, JobState newState)const noexcept {
         job.updateStatus(newState);
-        this->getDAG().getAdjacencyContainer().updateJobState(job);
+        persistJobState(job);
     }
 
     void persistJobState(const Job & job) const noexcept{
+        std::lock_guard lock {this->mutex};
         this->getDAG().getAdjacencyContainer().updateJobState(job);
     }
 
     auto onFinishedCallback() noexcept{
-        return [this](Job & job){
+        return [this](const Job & job){
+            std::cout << "Finished Job " << job.id << " (" << job.file.filename() << ") with state: "<<magic_enum::enum_name(job.state) << std::endl;
             this->persistJobState(job);
-            if(job.state == JobState::SUCCEED)
-                this->schedule();
         };
     }
 
+    bool hasRunnableJobs() const noexcept {
+        std::lock_guard lock {this->mutex};
+        const MemgraphConnection & connection = this->dag.getAdjacencyContainer().getConnection();
+        if(
+            Query("MATCH (j:Job {state:\"RUNNABLE\"})")
+            .line("RETURN j.id")
+            .execute(connection)
+        ){
+            auto result = connection->FetchAll();
+            return result.has_value() && result->size() > 0;
+        }   
+        return false;
+    }
 
+    bool jobsAreRunning() const noexcept {
+        std::lock_guard lock {this->mutex};
+        const MemgraphConnection & connection = this->dag.getAdjacencyContainer().getConnection();
+        if(
+            Query("MATCH (j:Job {state:\"RUNNING\"})")
+            .line("RETURN j.id")
+            .execute(connection)
+        ){
+            auto result = connection->FetchAll();
+            return result.has_value() && result->size() > 0;
+        }   
+        return false;
+    }
+    
 public:
     Scheduler(JobDAG_t && dag,Executor auto && executor,JobType lastJobType):dag(std::move(dag)),lastJobType(lastJobType){
          static_assert(std::convertible_to<decltype(this->onFinishedCallback()),Callback_t>);
@@ -53,15 +82,22 @@ public:
     }
 
     void schedule() {
-        auto jobsToSchedule =  dag.getNodes() | std::views::filter([this](Job & job){return canBeScheduled(job);});
-        std::vector<Job> jobsToRun;
-        for(auto && job : jobsToSchedule){
-            updateJobState(job,JobState::RUNNING);
-            jobsToRun.push_back(std::move(job));
+        while(hasRunnableJobs() || jobsAreRunning()){
+            std::vector<Job> jobsToRun;
+            for(auto & job: dag.getNodes()){
+                if(canBeScheduled(job)){
+                    jobsToRun.push_back(job);
+                    updateJobState(job,JobState::RUNNING);
+                }
+            }
+            for(const auto & job: jobsToRun) {
+                std::cout << "Starting Job "<< job.id << " (" << job.file.filename() <<")"<< std::endl;
+                executor(job);
+            }
+            sleep(1);
         }
-        for(auto & job: jobsToRun) {
-            executor(job);
-        }
+        std::cout <<"Shutting down scheduler" <<std::endl;
+        std::cout << std::endl;
     }
 
     JobDAG_t & getDAG() noexcept {
