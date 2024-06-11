c5e3a8b2ed507ea5f61b02323eb56f3bd7cf1529
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-11 17:17:05 +0200

    extracting scheduler implementation

---
+++ app/settlement_delineation_pattern_analysis/0_scheduler/Scheduler.hpp
@@ -0,0 +1,119 @@
+#pragma once
+#include <atomic>
+#include "JobDAG.hpp"
+#include "CwlToolExecutor.hpp"
+#include "Executor.hpp"
+
+
+class Scheduler {
+private:
+    mutable std::mutex mutex;
+    JobDAG_t dag;
+    Executor_t executor;
+    JobType lastJobType;
+    std::atomic_uint16_t activeThreads = 0;
+
+    static inline size_t THREAD_CONCURRENCY = std::thread::hardware_concurrency();
+
+    bool isFinished(const Job & job) const noexcept {
+        return job.state == JobState::FAILED || job.state == JobState::SUCCEED;
+    }
+
+    bool isRunnable(const Job & job) const noexcept {
+        return job.state == JobState::RUNNABLE;
+    }
+
+    bool canBeScheduled( Job & job) const noexcept {
+        if(job.type > lastJobType){
+            updateJobState(job,JobState::ABORTED);
+            return false;
+        }
+        if(not isRunnable(job) || activeThreads >= THREAD_CONCURRENCY)
+            return false;
+        return this->dag.inDegree(job) == 0 || std::ranges::all_of(dag.getReachableFrom(job),[this](const Job & parent){return this->isFinished(parent);});
+    }
+
+    void updateJobState(Job & job, JobState newState)const noexcept {
+        job.updateStatus(newState);
+        persistJobState(job);
+    }
+
+    void persistJobState(const Job & job) const noexcept{
+        this->getDAG().getAdjacencyContainer().updateJobState(job);
+    }
+
+    auto onFinishedCallback() noexcept{
+        return [this](const Job & job){
+            std::lock_guard lock {this->mutex};
+            std::cout << "Finished Job " << job.id << " (" << job.file.filename() << ") with state: "<<magic_enum::enum_name(job.state) << std::endl;
+            this->persistJobState(job);
+            this->activeThreads--;
+        };
+    }
+
+    bool hasRunnableJobs() const noexcept {
+        std::lock_guard lock {this->mutex};
+        const MemgraphConnection & connection = this->dag.getAdjacencyContainer().getConnection();
+        if(
+            Query("MATCH (j:Job {state:\"RUNNABLE\"})")
+            .line("RETURN j.id")
+            .execute(connection)
+        ){
+            auto result = connection->FetchAll();
+            return result.has_value() && result->size() > 0;
+        }   
+        return false;
+    }
+
+    bool jobsAreRunning() const noexcept {
+        std::lock_guard lock {this->mutex};
+        const MemgraphConnection & connection = this->dag.getAdjacencyContainer().getConnection();
+        if(
+            Query("MATCH (j:Job {state:\"RUNNING\"})")
+            .line("RETURN j.id")
+            .execute(connection)
+        ){
+            auto result = connection->FetchAll();
+            return result.has_value() && result->size() > 0;
+        }   
+        return false;
+    }
+    
+public:
+    Scheduler(JobDAG_t && dag,Executor auto && executor,JobType lastJobType):dag(std::move(dag)),lastJobType(lastJobType){
+         static_assert(std::convertible_to<decltype(this->onFinishedCallback()),Callback_t>);
+         executor.setCallback(onFinishedCallback());
+         this->executor = std::move(executor);
+    }
+
+    void schedule() {
+        while(hasRunnableJobs() || jobsAreRunning()){
+            std::vector<Job> jobsToRun;
+            {
+                std::lock_guard lock {this->mutex};
+                for(auto & job: dag.getNodes()){
+                    if(canBeScheduled(job)){
+                        activeThreads++;
+                        jobsToRun.push_back(job);
+                        updateJobState(job,JobState::RUNNING);
+                    }
+                }
+            }
+            for(const auto & job: jobsToRun) {
+                std::cout << "Starting Job "<< job.id << " (" << job.file.filename() <<")"<< std::endl;
+                executor(job);
+            }
+            sleep(1);
+        }
+        std::cout <<"Shutting down scheduler" <<std::endl;
+        std::cout << std::endl;
+    }
+
+    JobDAG_t & getDAG() noexcept {
+        return this->dag;
+    }
+
+    const JobDAG_t & getDAG() const noexcept {
+        return this->dag;
+    }
+};
---
+++ app/settlement_delineation_pattern_analysis/0_scheduler/SchedulerConfig.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <magic_enum.hpp>
+#include <fishnet/FunctionalConcepts.hpp>
+#include "TaskConfig.hpp"
+#include "Job.hpp"
+#include "CwlToilExecutor.hpp"
+#include "CwlToolExecutor.hpp"
+#include "Scheduler.hpp"
+
+class SchedulerConfig:public MemgraphTaskConfig{
+private:
+    constexpr static const char * LAST_JOB_TYPE_KEY = "last-job-type";
+    constexpr static const char * EXECUTOR_KEY = "executor";
+public:
+    JobType lastJobType;
+    ExecutorType executorType;
+    json executorDesc;
+
+    SchedulerConfig(const json & config):MemgraphTaskConfig(config){
+        const auto & cfg = this->jsonDescription;
+        std::string jobTypeName;
+        cfg.at(LAST_JOB_TYPE_KEY).get_to(jobTypeName);
+        auto asJobType = magic_enum::enum_cast<JobType>(jobTypeName);
+        if(not asJobType)
+            lastJobType = JobType::ANALYSIS;
+        else 
+            lastJobType = asJobType.value();
+        std::string executorTypename;
+        cfg.at(EXECUTOR_KEY).at("name").get_to(executorTypename);
+        auto executorOpt = magic_enum::enum_cast<ExecutorType>(executorTypename);
+        if(not executorOpt)
+            throw std::runtime_error("Could not parse executor type from json\n"+cfg.dump());
+        this->executorType = executorOpt.value();
+        executorDesc = cfg.at(EXECUTOR_KEY);
+    }
+
+    Scheduler getSchedulerWithExecutorType (JobDAG_t && dag) {
+        switch(executorType){
+            case ExecutorType::CWLTOIL: {
+                std::string cwlDirectory;
+                executorDesc.at("cwl-directory").get_to(cwlDirectory);
+                std::string flags;
+                executorDesc.at("flags").get_to(flags);
+                return Scheduler(std::move(dag),CwlToilExecutor(std::move(cwlDirectory),std::move(flags)),lastJobType);
+            }
+            case ExecutorType::CWLTOOL:{
+                std::string cwlDir;
+                executorDesc.at("cwl-directory").get_to(cwlDir);
+                return Scheduler(std::move(dag),CwlToolExecutor(std::move(cwlDir)),lastJobType);
+            }
+            default:
+                throw std::runtime_error("Could not create scheduler from executor type.\n"+this->jsonDescription.dump());
+        }
+    }
+};
