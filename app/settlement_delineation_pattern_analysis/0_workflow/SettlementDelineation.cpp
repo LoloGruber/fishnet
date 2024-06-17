6fedeb7d6ef9372aa022b56441ff7810197330b9
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-18 01:40:12 +0200

    testing whole workflow with merge step

--- app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.cpp
+++ app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.cpp
@@ -8,16 +8,33 @@
 int main(int argc, char * argv[]){
     CLI::App app {"SettlementDelineation Workload-Generator and Scheduler"};
     std::string pathToCfg = "/home/lolo/Documents/fishnet/app/settlement_delineation_pattern_analysis/cfg/workflow.json";
-    std::string inputDirectory = "/home/lolo/Documents/fishnet/data/testing/Punjab_Split";
+    std::string inputDirectory = "/home/lolo/Documents/fishnet/data/testing/Punjab_Small_Split";
+    std::string outputFilename;
     std::string workingDirectory;
     std::vector<std::filesystem::path> inputFiles;
     app.add_option("-c,--config",pathToCfg,"Path to workflow.json configuration")->required()->check(CLI::ExistingFile);
     app.add_option("-i,--inputDirectory",inputDirectory,"Path to input directory")->required()->check(CLI::ExistingDirectory);
     app.add_option("-d,--workingDirectory",workingDirectory,"Path to working directory [default: current directory]")->check(CLI::ExistingDirectory);
+    app.add_option("-o,--output",outputFilename,"Path where merged output will be stored (only applied if last job type is at least MERGE)")->check([](const std::string & str){
+        try{
+            auto file = fishnet::Shapefile(str);
+            return std::string();
+        }catch(std::invalid_argument & error){
+            return std::string("Invalid output path:\n"+str+"\n")+ error.what();
+        }
+    });
     CLI11_PARSE(app,argc,argv);
+    std::filesystem::path workingDir;
     if(workingDirectory.empty())
-        workingDirectory = std::filesystem::current_path().string();
-    SettlementDelineation task {json::parse(std::ifstream(pathToCfg)),fishnet::GISFactory::getGISFiles(inputDirectory),{workingDirectory}};
+        workingDir = std::filesystem::current_path().string();
+    else 
+        workingDir = {workingDirectory};
+    std::filesystem::path output;
+    if(outputFilename.empty())
+        output = workingDir / std::filesystem::path("Result.shp");
+    else   
+        output = outputFilename;    
+    SettlementDelineation task {json::parse(std::ifstream(pathToCfg)),fishnet::GISFactory::getGISFiles(inputDirectory),output,workingDir};
     task.run();
     return 0;
 }
--- app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.h
+++ app/settlement_delineation_pattern_analysis/0_workflow/SettlementDelineation.h
@@ -1,23 +1,26 @@
 #pragma once
 #include <vector>
 #include <filesystem>
-
+#include <fishnet/GISFactory.hpp>
 #include "Task.hpp"
 #include "JobDAG.hpp"
 
 #include "JobGenerator.hpp"
 #include "SchedulerConfig.hpp"
+#include "ConnectedComponentsConfig.hpp"
 
 class SettlementDelineation: public Task{
 private:
     SchedulerConfig schedulerConfig;
     JobGeneratorConfig jobGeneratorConfig;
+    ConnectedComponentsConfig connectedComponentsConfig;
     std::vector<std::filesystem::path> inputFiles;
     std::filesystem::path workingDirectory;
+    std::filesystem::path outputPath;
 
 public:
-    SettlementDelineation(const json & cfg, std::vector<std::filesystem::path> && inputFiles,std::filesystem::path workingDirectory = std::filesystem::current_path())
-    :schedulerConfig(cfg),jobGeneratorConfig(cfg),inputFiles(std::move(inputFiles)),workingDirectory(std::move(workingDirectory)){
+    SettlementDelineation(const json & cfg, std::vector<std::filesystem::path> && inputFiles,std::filesystem::path outputPath, std::filesystem::path workingDirectory = std::filesystem::current_path())
+    :schedulerConfig(cfg),jobGeneratorConfig(cfg),connectedComponentsConfig(cfg),inputFiles(std::move(inputFiles)),workingDirectory(std::move(workingDirectory)),outputPath(std::move(outputPath)){
         this->writeDescLine("Settlement Delineation Workload Generator & Scheduler:")
         .writeDescLine("Config:")
         .writeDescLine(cfg.dump(4))
@@ -25,6 +28,8 @@ public:
         .indentDescLine(this->workingDirectory.string())
         .writeDescLine("Inputs:");
         std::ranges::for_each(this->inputFiles,[this](const auto & file){this->indentDescLine(file.string());});
+        this->writeDescLine("Output:")
+        .indentDescLine(this->outputPath.string());
     }
 
     void run() override {
@@ -33,9 +38,26 @@ public:
         auto exp = MemgraphConnection::create(schedulerConfig.params).transform([](auto && conn){return JobAdjacency(std::move(conn));});
         auto && jobAdj = getExpectedOrThrowError(exp);
         auto jobDAG = loadDAG(std::move(jobAdj));
-        JobGenerator jobGenerator {std::move(jobGeneratorConfig),workingDirectory};
+        JobGeneratorConfig copy = jobGeneratorConfig;
+        JobGenerator jobGenerator {std::move(copy),workingDirectory};
         jobGenerator.generate(inputFiles,jobDAG);
         Scheduler scheduler = schedulerConfig.getSchedulerWithExecutorType(std::move(jobDAG));
         scheduler.schedule();
+        if(schedulerConfig.lastJobType >= JobType::MERGE){
+            MergeJob mergeJob;
+            mergeJob.id=(size_t)-1;
+            mergeJob.file = jobGeneratorConfig.jobDirectory / "Merge.json";
+            mergeJob.state = JobState::RUNNABLE;
+            for(auto && path : fishnet::GISFactory::getGISFiles(workingDirectory)){
+                if(path.stem().string().starts_with(connectedComponentsConfig.analysisOutputStem) && path.string().ends_with(".shp")){
+                    mergeJob.inputs.push_back(std::move(path));
+                }
+            }
+            mergeJob.output = outputPath;
+            JobWriter::write(mergeJob);
+            scheduler.getDAG().addNode(mergeJob);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            scheduler.schedule();
+        }
     }
 };
