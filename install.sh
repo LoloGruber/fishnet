92b2bee85aed24934f65447770ee51a5d0fe0693
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-06-30 18:42:02 +0200

    do not build tests by default in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=ON -DFISHNET_APPS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
+cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=OFF -DFISHNET_APPS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 cmake --build . --config Release -j 16
 cmake --install .
 cd ..
