3a84c8699b8ac0b91f9e863c804f0de95d303b59
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-11-20 18:21:35 +0100

    sda workflow now uses temporary directory some refactoring

--- app/sda-workflow/src/0_workflow/SettlementDelineation.cpp
+++ app/sda-workflow/src/0_workflow/SettlementDelineation.cpp
@@ -1,45 +1,49 @@
 #include "SettlementDelineation.h"
 #include <nlohmann/json.hpp>
 #include <CLI/CLI.hpp>
-#include <fishnet/GISFactory.hpp>
-
 
 
 int main(int argc, char * argv[]){
     CLI::App app {"SettlementDelineation Workload-Generator and Scheduler"};
     std::string pathToCfg = "/home/lolo/Documents/fishnet/prod/local/sda-workflow-local.json";
-    std::string inputDirectory = "/home/lolo/Documents/fishnet/data/WSF/2019/Punjab";
+    std::string inputPath = "/home/lolo/Documents/fishnet/data/WSF/2019/Punjab";
     std::string outputFilename;
-    std::string workingDirectory = "/home/lolo/Documents/fishnet/tests/workflow/test/workingDirectory";
+    std::string workingDirectoryName /* = "/home/lolo/Documents/fishnet/tests/workflow/test/workingDirectory" */;
     std::vector<std::filesystem::path> inputFiles;
     app.add_option("-c,--config",pathToCfg,"Path to workflow configuration")->required()->check(CLI::ExistingFile);
-    app.add_option("-i,--inputDirectory",inputDirectory,"Path to input directory")->required()->check(CLI::ExistingDirectory);
-    app.add_option("-d,--workingDirectory",workingDirectory,"Path to working directory [default: current directory]")->check(CLI::ExistingDirectory);
-    app.add_option("-o,--output",outputFilename,"Path where merged output will be stored (only applied if last job type is at least MERGE)")->check([](const std::string & str){
+    app.add_option("-i,--input",inputPath,"Path to input directory")->required()->check(CLI::ExistingDirectory | CLI::ExistingFile);
+    app.add_option("-d,--workingDirectory",workingDirectoryName,"Path to working directory [default: temporary directory]")->check(CLI::ExistingDirectory);
+    app.add_option("-o,--output",outputFilename,"Path where merged output will be stored (only applied if last job type is at least MERGE)")->required()->check([](const std::string & str){
         try{
-            auto file = fishnet::Shapefile(str);
+            auto file = fishnet::Shapefile(str); // try to create shapefile to ensure the correct extension
             std::filesystem::path parentPath = std::filesystem::path(str).parent_path();
+            if (str.empty()) {
+                return std::string("Output path is empty");
+            }
+            // Try creating the parent directories (if they don't exist)
+            if (!parentPath.empty() && !std::filesystem::exists(parentPath)) {
+                std::filesystem::create_directories(parentPath); // Create missing directories
+            }
+            // Check if the directory creation succeeded or if the path is already valid
+            if (std::filesystem::exists(parentPath) && !std::filesystem::is_directory(parentPath)) {
+                return std::string("Path \""+str+ "\" is not a valid directory");
+            }
+
             if(not std::filesystem::exists(parentPath))
                 return std::string("Path to output file (\""+str+"\") does not exist");
             return std::string();
-        }catch(std::invalid_argument & error){
-            return std::string("Invalid output path:\n"+str+"\n")+ error.what();
+        }catch(std::invalid_argument & shpError){
+            return std::string("Invalid output shapefile extension:\n"+str+"\n")+ shpError.what();
+        }
+        catch(std::filesystem::filesystem_error & fsError ){
+            return std::string("Invalid output path:\n"+str+"\n")+ fsError.what();
         }
     });
     CLI11_PARSE(app,argc,argv);
-    std::filesystem::path workingDir;
-    if(workingDirectory.empty())
-        workingDir = std::filesystem::current_path();
-    else {
-        workingDir = fishnet::util::PathHelper::absoluteCanonical(workingDirectory);
-        std::filesystem::current_path(workingDir);
+    if(not workingDirectoryName.empty()){
+        fishnet::util::TemporaryDirectory::setTmpPrefix(fishnet::util::PathHelper::absoluteCanonical(workingDirectoryName)); // change prefix for tmp directory
     }
-    std::filesystem::path output;
-    if(outputFilename.empty())
-        output = workingDir / std::filesystem::path(workingDir.stem()).replace_extension(".shp");
-    else   
-        output = outputFilename;    
-    SettlementDelineation task {json::parse(std::ifstream(pathToCfg)),fishnet::GISFactory::getGISFiles(inputDirectory),output,{pathToCfg},workingDir};
+    SettlementDelineation task {json::parse(std::ifstream(pathToCfg)),{inputPath},{outputFilename},{pathToCfg}};
     task.run();
     return 0;
 }
--- app/sda-workflow/src/0_workflow/SettlementDelineation.h
+++ app/sda-workflow/src/0_workflow/SettlementDelineation.h
@@ -16,15 +16,17 @@ private:
     ConnectedComponentsConfig connectedComponentsConfig;
     std::vector<std::filesystem::path> inputFiles;
     std::filesystem::path cfgFile;
-    std::filesystem::path workingDirectory;
     std::filesystem::path outputPath;
+    fishnet::util::TemporaryDirectory workingDirectory;
 
 public:
-    SettlementDelineation(const json & cfg, std::vector<std::filesystem::path> && inputFiles,std::filesystem::path outputPath, std::filesystem::path cfgFile, std::filesystem::path workingDirectory = std::filesystem::current_path())
-    :schedulerConfig(cfg),jobGeneratorConfig(cfg),connectedComponentsConfig(cfg),inputFiles(std::move(inputFiles)),cfgFile(std::move(cfgFile)),workingDirectory(std::move(workingDirectory)),outputPath(std::move(outputPath)){
+    SettlementDelineation(const json & cfg,const std::filesystem::path & inputPath,std::filesystem::path outputPath, std::filesystem::path configPath)
+    :schedulerConfig(cfg),jobGeneratorConfig(cfg),connectedComponentsConfig(cfg),cfgFile(std::move(configPath)),outputPath(std::move(outputPath)){
+        this->inputFiles = fishnet::GISFactory::getGISFiles(inputPath);
+        std::filesystem::current_path(workingDirectory); // set current path to working directory
         this->desc["type"]="Settlement Delineation Workload Generator & Scheduler";
         this->desc["config"] = cfg;
-        this->desc["working-directory"]=this->workingDirectory.string();
+        this->desc["working-directory"]=this->workingDirectory.get().string();
         std::vector<std::string> inputStrings;
         std::ranges::for_each(this->inputFiles,[this,&inputStrings](const auto & file){inputStrings.push_back(file.string());});
         this->desc["inputs"]=inputStrings;
@@ -34,23 +36,18 @@ public:
     void run() override {
         if(inputFiles.empty())
             throw std::runtime_error("No input files provided");
-        auto exp = MemgraphConnection::create(schedulerConfig.params).transform([](auto && conn){return JobAdjacency(std::move(conn));});
-        auto && jobAdj = getExpectedOrThrowError(exp);
-        Scheduler scheduler = schedulerConfig.getSchedulerWithExecutorType(loadDAG(std::move(jobAdj)));
+        if(not std::filesystem::is_empty(workingDirectory))
+            throw std::runtime_error("Working directory is not empty");
+        Scheduler scheduler = schedulerConfig.getSchedulerWithExecutorType(loadDAG(JobAdjacency(MemgraphConnection::create(schedulerConfig.params).value_or_throw())));
         auto & jobDag = scheduler.getDAG();
-        JobGeneratorConfig copy = jobGeneratorConfig;
-        JobGenerator jobGenerator {std::move(copy),workingDirectory,cfgFile};
-        auto pathToTmpDir = workingDirectory / std::filesystem::path("tmp/");
-        if (std::filesystem::exists(pathToTmpDir)) {
-            std::filesystem::remove_all(pathToTmpDir);
-        }
-        auto tmpDir = fishnet::util::TemporaryDirectory(pathToTmpDir);
+        JobGenerator jobGenerator {JobGeneratorConfig(jobGeneratorConfig),workingDirectory,cfgFile};
+        fishnet::util::AutomaticTemporaryDirectory tmp;
         if(jobGeneratorConfig.cleanup)
             jobGenerator.cleanup(jobDag);
         if(jobGeneratorConfig.splits > 0){
-            jobGenerator.generateWSFSplitJobs(inputFiles,tmpDir.getDirectory(),jobDag);
+            jobGenerator.generateWSFSplitJobs(inputFiles,tmp,jobDag);
             scheduler.schedule();
-            jobGenerator.generate(fishnet::GISFactory::getGISFiles(tmpDir.getDirectory()),jobDag);
+            jobGenerator.generate(fishnet::GISFactory::getGISFiles(tmp),jobDag);
         }else {
             jobGenerator.generate(inputFiles,jobDag);
         }
@@ -83,6 +80,7 @@ public:
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             scheduler.schedule();
         }
+        //TODO clear workingdirectory if cfg enables and no failed jobs
         json cfg = json::parse(std::ifstream(cfgFile));
         std::ofstream cfgStream {fishnet::util::PathHelper::appendToFilename(outputPath,"_cfg.json")};
         cfgStream << cfg.dump(4) << std::endl;
