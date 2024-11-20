929d64055f560a0e61f3352cd47b0e457c0d80c9
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-11-20 15:47:42 +0100

    changing interface of temporary directory

--- lib/util/filesystem/src/TemporaryDirectory.cpp
+++ lib/util/filesystem/src/TemporaryDirectory.cpp
@@ -1,20 +1,64 @@
 #include <fishnet/TemporaryDirectiory.h>
 #include <stdexcept>
+#include <random>
 
 namespace fishnet::util{
-TemporaryDirectory::TemporaryDirectory(const std::filesystem::path & directory):directory(directory){
-        if (std::filesystem::exists(directory) && not std::filesystem::is_empty(directory)){
-            throw std::invalid_argument("Directory already exists and is not empty");
-        }
-        std::filesystem::create_directory(directory);
+
+std::filesystem::path TemporaryDirectory::fromID(size_t id) noexcept {
+    return TMP_PREFIX / std::filesystem::path(std::to_string(id)+"/");
 }
 
-std::filesystem::path TemporaryDirectory::getDirectory()const noexcept{
+size_t TemporaryDirectory::randomUniqueID() noexcept {
+    static std::random_device rd;
+    static std::mt19937 generator(rd());
+    static std::uniform_int_distribution<size_t> distribution(0,std::numeric_limits<size_t>::max());
+    size_t id = distribution(generator);
+    while(std::filesystem::exists(TemporaryDirectory::fromID(id))) {
+        id = distribution(generator);
+    }
+    return id;
+}
+
+std::optional<TemporaryDirectory> TemporaryDirectory::load(size_t id) noexcept{
+    auto path = TemporaryDirectory::fromID(id);
+    if(std::filesystem::exists(path))
+        return std::optional<TemporaryDirectory>(TemporaryDirectory(id,path));
+    return std::nullopt;
+}
+
+TemporaryDirectory::TemporaryDirectory() :identifier(randomUniqueID()),directory(fromID(identifier)){
+    init();
+}
+
+TemporaryDirectory::operator const std::filesystem::path & () const noexcept{
+    return this->get();
+}
+
+const std::filesystem::path & TemporaryDirectory::get()const noexcept{
     return this->directory;
 }
 
-TemporaryDirectory::~TemporaryDirectory(){
+size_t TemporaryDirectory::id() const noexcept{
+    return this->identifier;
+}
+
+void TemporaryDirectory::clear() const noexcept {
     std::filesystem::remove_all(directory);
     std::filesystem::remove(directory);
+}
+
+bool TemporaryDirectory::init() const noexcept {
+    if(std::filesystem::exists(this->directory))
+        return false;
+    std::filesystem::create_directories(directory);
+    return true;
+}
+
+std::optional<AutomaticTemporaryDirectory> AutomaticTemporaryDirectory::load(size_t id) noexcept {
+    return TemporaryDirectory::load(id).transform([](auto&& tmp){return static_cast<AutomaticTemporaryDirectory>(tmp);});
+}
+
+AutomaticTemporaryDirectory::~AutomaticTemporaryDirectory(){
+    this->clear();
 }
 }
