35794f0d2a7d151029c9de74556e8fc2b9a104bd
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-11-21 19:29:32 +0100

    independent build for release

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-rm -rf build
-mkdir build
-cd build
+BUILD_DIRECTORY="build-release"
+rm -rf $BUILD_DIRECTORY
+mkdir $BUILD_DIRECTORY
+cd $BUILD_DIRECTORY
 cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=OFF -DFISHNET_APPS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 cmake --build . --config Release -j 16
 cmake --install .
 cd ..
-rm -rf build
+rm -rf $BUILD_DIRECTORY
