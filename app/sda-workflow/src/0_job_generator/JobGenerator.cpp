79c04961cbcb0757af10279cae3aca4a78ccb0a2
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-11-21 19:17:06 +0100

    adding workflow id option to job generator

--- app/sda-workflow/src/0_job_generator/JobGenerator.cpp
+++ app/sda-workflow/src/0_job_generator/JobGenerator.cpp
@@ -9,9 +9,11 @@ int main(int argc, char * argv[]){
     std::string workingDirectoryName;
     std::string inputDirectory;
     std::string configPath;
+    size_t workflowID = 0;
     generatorApp.add_option("-c,--config",configPath,"Path to job generator / workflow configuration file")->required()->check(CLI::ExistingFile);
     generatorApp.add_option("-i,--inputDirectory",inputDirectory,"Path to input directory of GIS Files")->required()->check(CLI::ExistingDirectory);
     generatorApp.add_option("-d,--workingDirectory",workingDirectoryName,"Working Directory for the workflow")->check(CLI::ExistingDirectory);
+    generatorApp.add_option("-w,--workflowID",workflowID,"Unique workflow id (optional)")->check(CLI::PositiveNumber);
     CLI11_PARSE(generatorApp,argc,argv);
     auto cfg = JobGeneratorConfig(nlohmann::json::parse(std::ifstream(configPath)));
     std::filesystem::path workingDirectory;
@@ -20,7 +22,7 @@ int main(int argc, char * argv[]){
     }else {
         workingDirectory = std::filesystem::path(workingDirectoryName);
     }
-    auto exp = MemgraphConnection::create(cfg.params).transform([](auto && conn){return JobAdjacency(std::move(conn));});
+    auto exp = MemgraphConnection::create(cfg.params,workflowID).transform([](auto && conn){return JobAdjacency(std::move(conn));});
     auto && jobAdj = getExpectedOrThrowError(exp);
     auto jobDAG = loadDAG(std::move(jobAdj));
     JobGenerator jobGenerator {std::move(cfg),std::move(workingDirectory),{configPath}};
