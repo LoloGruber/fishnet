85872fad5a3228a1c16401097df0267eaa67f849
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-11-25 11:43:41 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 BUILD_DIRECTORY="build-release"
-# rm -rf $BUILD_DIRECTORY
+rm -rf $BUILD_DIRECTORY
 mkdir $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY
 cmake -DCMAKE_BUILD_TYPE=Release -DFISHNET_TEST=OFF -DFISHNET_APPS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 cmake --build . --config Release -j 16
 cmake --install .
 cd ..
-# rm -rf $BUILD_DIRECTORY
+rm -rf $BUILD_DIRECTORY
