4c13d1eccf01a7173aaf2d5f86bbab6222fb23c5
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2024-12-17 16:58:12 +0100

    adapting neighbouring predicate reader

--- app/sda-workflow/src/shared/neighbourPredicates/NeighbourPredicateJsonReader.hpp
+++ app/sda-workflow/src/shared/neighbourPredicates/NeighbourPredicateJsonReader.hpp
@@ -6,12 +6,12 @@
 #include "DistanceBiPredicate.hpp"
 
 template<typename T>
-static std::optional<fishnet::util::BiPredicate_t<T>> fromNeighbouringPredicateType(NeighbouringPredicateType type,const nlohmann::json & predicateDesc) {
+static std::optional<fishnet::util::BiPredicate_t<T>> fromNeighbouringPredicateType(NeighbouringPredicateType type,const nlohmann::json & predicateDesc, const DistanceFunction & distanceFunction = MetricDistance()) {
     switch(type){
         case NeighbouringPredicateType::DistanceBiPredicate:
             double maxDistance;
             predicateDesc.at("distance").get_to(maxDistance);
-            return DistanceBiPredicate(maxDistance);
+            return DistanceBiPredicate(distanceFunction,maxDistance);
     }
     return std::nullopt;
 }
