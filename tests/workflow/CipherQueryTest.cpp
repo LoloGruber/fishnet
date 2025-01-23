f43764f56b6ad27877373286983f2ac185b039a1
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2025-01-23 17:04:32 +0100

    renaming tests

--- tests/workflow/CipherQueryTest.cpp
+++ tests/workflow/CipherQueryTest.cpp
@@ -5,7 +5,7 @@
 
 using namespace testutil;
 
-class MemgraphQueryTest: public ::testing::Test {
+class CipherQueryTest: public ::testing::Test {
 protected:
     void SetUp() override {
         auto connectionExp = MemgraphConnection::create(hostname,port);
@@ -24,11 +24,11 @@ protected:
     MemgraphConnection connection;
 };
 
-TEST_F(MemgraphQueryTest, init){
+TEST_F(CipherQueryTest, init){
     EXPECT_NO_FATAL_FAILURE(auto q = CipherQuery());
 }
 
-TEST_F(MemgraphQueryTest, mergeAndMatchNode){
+TEST_F(CipherQueryTest, mergeAndMatchNode){
     EXPECT_TRUE(connection.executeAndDiscard(CipherQuery().merge(Node{.label=Label::Settlement,.attributes="id:1,value:\"Test\""})));
     EXPECT_TRUE(connection.executeAndDiscard(CipherQuery().merge(Node{.label=Label::Settlement,.attributes="id:2"})));
     EXPECT_TRUE(connection.execute(CipherQuery().match(Node{.name="n",.label=Label::Settlement,.attributes="id:1,value:\"Test\""}).ret("n")));
@@ -36,8 +36,9 @@ TEST_F(MemgraphQueryTest, mergeAndMatchNode){
     EXPECT_EQ(resultSize,1); //only one node expected with id 1
 }
 
-TEST_F(MemgraphQueryTest, mergeAndMatchEdge){
-    CipherQuery i = CipherQuery().merge(Node{.name="f",.label=Label::Settlement,.attributes="id:1"}).endl();
+TEST_F(CipherQueryTest, mergeAndMatchEdge){
+    CipherQuery i;
+    i.merge(Node{.name="f",.label=Label::Settlement,.attributes="id:1"}).endl();
     i.merge(Node{.name="t",.label=Label::Settlement,.attributes="id:2"}).endl();
     i.merge(Relation{.from=Var("f"),.label=Label::neighbours,.to=Var("t")});
     EXPECT_TRUE(connection.executeAndDiscard(i));
@@ -47,7 +48,7 @@ TEST_F(MemgraphQueryTest, mergeAndMatchEdge){
     EXPECT_EQ(resultSize,1);
 }
 
-TEST_F(MemgraphQueryTest, parameterizedQuery){
+TEST_F(CipherQueryTest, parameterizedQuery){
     size_t nodeID = 5;
     CipherQuery i = CipherQuery().merge(Node{.label=Label::Settlement,.attributes="id:$nodeID"}).setInt("nodeID",nodeID);
     i.merge(Node{.name="n",.label=Label::Settlement,.attributes="id:-1"});
@@ -62,7 +63,7 @@ TEST_F(MemgraphQueryTest, parameterizedQuery){
     EXPECT_EQ(resultSize,2);
 }
 
-TEST_F(MemgraphQueryTest, compositeEdge) {
+TEST_F(CipherQueryTest, compositeEdge) {
     CipherQuery i = CipherQuery().merge(Node{.name="n",.label=Label::Settlement,.attributes="id:1"}).endl();
     i.merge(Node{.name="f",.label=Label::File,.attributes="path:\"Test.shp\""}).endl();
     i.merge(Node{.name="c",.label=Label::Component}).endl();
