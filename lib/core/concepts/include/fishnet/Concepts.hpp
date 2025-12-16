dca3ef29af5e4cbf803192d010f728a36d1850c1
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2025-12-16 15:50:06 +0100

    adding concepts lib

---
+++ lib/core/concepts/include/fishnet/Concepts.hpp
@@ -0,0 +1,3 @@
+#include <fishnet/Functional.hpp>
+#include <fishnet/CollectionsConcepts.hpp>
+// #include <fishnet/Printable.hpp>
--- lib/core/util/include/fishnet/UtilConcepts.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <fishnet/CollectionConcepts.hpp>
-#include <fishnet/HashConcepts.hpp>
-#include <fishnet/FunctionalConcepts.hpp>
-#include <fishnet/Printable.hpp>
