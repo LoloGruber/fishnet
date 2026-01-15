30781578e34bfbbff6826406af1eaaf4270841d2
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-01-15 15:30:24 +0100

    moving distance function to workflow lib

---
+++ lib/workflow/spatial-clustering/include/fishnet/DistanceBiPredicate.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "DistanceFunction.hpp"
+
+
+/**
+ * @brief Distance BiPredicate Functor.
+ * Emits true if the distance between the two shapes is less or equal to the maximum distance (in meters).
+ * @tparam DistanceFunction type determining how to calculate the distance between two points in meters.
+ */
+template<fishnet::util::BiFunction<fishnet::geometry::Vec2DReal,fishnet::geometry::Vec2DReal, fishnet::math::DEFAULT_FLOATING_POINT> DistanceFunction>
+struct DistanceBiPredicate{
+    DistanceFunction distanceFunction;
+    double maxDistanceInMeters;
+
+    bool operator()(fishnet::geometry::Shape auto const & lhs, fishnet::geometry::Shape auto const & rhs) const noexcept {
+        auto [l,r] = fishnet::geometry::closestPoints(lhs,rhs);
+        return l == r || distanceFunction(l,r) <= maxDistanceInMeters;
+    }      
+};
+
