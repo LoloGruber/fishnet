5cd23c9d73bcbebbc4a6bee6358647dc93be8b6e
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-01-15 16:19:19 +0100

    fixing include

--- app/sda-workflow/src/shared/neighbourPredicates/NeighbourPredicateJsonReader.hpp
+++ app/sda-workflow/src/shared/neighbourPredicates/NeighbourPredicateJsonReader.hpp
@@ -3,7 +3,7 @@
 #include <nlohmann/json.hpp>
 #include <fishnet/FunctionalConcepts.hpp>
 #include "NeighbourPredicateType.hpp"
-#include "DistanceBiPredicate.hpp"
+#include <fishnet/DistanceBiPredicate.hpp>
 
 template<typename T>
 static std::optional<fishnet::util::BiPredicate_t<T>> fromNeighbouringPredicateType(NeighbouringPredicateType type,const nlohmann::json & predicateDesc, const DistanceFunction & distanceFunction = MetricDistance()) {
