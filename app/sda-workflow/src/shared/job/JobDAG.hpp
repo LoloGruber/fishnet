c0dbf5686b080b6dba28cd0b97851ac28578a94c
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-02-03 15:11:48 +0100

    removing sda workflow from fishnet framework

--- app/sda-workflow/src/shared/job/JobDAG.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <fishnet/Graph.hpp>
-#include <JobAdjacency.hpp>
-
-using JobDAG_t = decltype(fishnet::graph::GraphFactory::DAG<Job>(JobAdjacency(MemgraphConnection::create({}).value())));
-
-static JobDAG_t loadDAG(JobAdjacency && adj) {
-    return fishnet::graph::GraphFactory::DAG<Job>(std::move(adj));
-}
