f3187b38a80086b4ab72147608ad481c69cae9d4
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-02-18 17:38:49 +0100

    create file in cwd

--- app/common/ShapefileMerger.cpp
+++ app/common/ShapefileMerger.cpp
@@ -44,11 +44,11 @@ int main(int argc, char * argv[]){
     });
     app.add_option("-o,--output",outputFilename,"Output file location")->required()->check([](const std::string & str){
         try{
-            auto file = fishnet::Shapefile(str); //TODO use current working directory / filename.shp to prevent cwl error in readonly containers
-            std::filesystem::path parentPath = std::filesystem::path(str).parent_path();
-            if(not std::filesystem::exists(parentPath)){
-                std::filesystem::create_directories(parentPath);
-            }
+            auto file = fishnet::Shapefile(std::filesystem::path(str).filename()); //TODO use current working directory / filename.shp to prevent cwl error in readonly containers
+            // std::filesystem::path parentPath = std::filesystem::path(str).parent_path();
+            // if(not std::filesystem::exists(parentPath)){
+            //     std::filesystem::create_directories(parentPath);
+            // }
             return std::string();
         }catch(std::invalid_argument & error){
             return std::string("Invalid output path:\n"+str+"\n")+ error.what();
