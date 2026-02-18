fd5b77970a4968cca0f6fb7d88d29641facdb8bf
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-02-18 18:28:04 +0100

    updater for fishnet dependencies

--- prod/docker/update.sh
+++ prod/docker/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker build -t logru/fishnet-deps:latest .
+VERSION=1.1.0
+docker build -t logru/fishnet-deps:$VERSION -t logru/fishnet-deps:latest -f Dockerfile.deps .
 docker login 
+docker push logru/fishnet-deps:$VERSION 
 docker push logru/fishnet-deps:latest
