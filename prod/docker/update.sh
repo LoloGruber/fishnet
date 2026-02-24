d6f4f95dc9e411f314f65398ba4ea93057e87f8c
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-02-24 15:13:50 +0100

    adding docker image for fishnet binaries

--- prod/docker/update.sh
+++ prod/docker/update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-VERSION=1.1.0
-docker build -t logru/fishnet-deps:$VERSION -t logru/fishnet-deps:latest -f Dockerfile.deps .
-docker login 
-docker push logru/fishnet-deps:$VERSION 
-docker push logru/fishnet-deps:latest
+PATH_TO_PROJECT_ROOT=../../
+VERSION=$(grep -oP 'project\s*\([^)]*VERSION\s+\K[0-9]+\.[0-9]+\.[0-9]+' $PATH_TO_PROJECT_ROOT/CMakeLists.txt)
+IMAGE=fishnet-apps
+docker build -t logru/$IMAGE:$VERSION -t logru/$IMAGE:latest -f Dockerfile.apps $PATH_TO_PROJECT_ROOT
+# docker login 
+# docker push logru/$IMAGE:$VERSION 
+# docker push logru/$IMAGE:latest
+
+# Update fishnet-deps
+# IMAGE=fishnet-deps
+# docker build -t logru/$IMAGE:$VERSION -t logru/$IMAGE:latest -f Dockerfile.deps $PATH_TO_PROJECT_ROOT
+# docker push logru/$IMAGE:$VERSION
+# docker push logru/$IMAGE:latest
