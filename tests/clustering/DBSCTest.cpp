6e1db5e619d69105523bad31d78207d695f75eef
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-03-09 14:22:18 +0100

    fixing compile

--- tests/clustering/DBSCTest.cpp
+++ tests/clustering/DBSCTest.cpp
@@ -32,6 +32,6 @@ protected:
 
  TEST_F(DBSCTest, DefaultClusteringNoAttribute){
     auto distanceFunction = [](const auto & lhs, const auto & rhs){return lhs.distance(rhs);};
-    fishnet::DBSC<T> dbsc(2, distanceFunction);
+    fishnet::DBSC<T> dbsc(4.0,2, distanceFunction);
     auto result = dbsc.cluster(graph);
  }
