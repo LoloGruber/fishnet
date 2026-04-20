a78df4fe98a7491a5dcfdb955a29c4e775cb57f9
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-04-20 17:12:02 +0200

    fixing include

--- lib/workflow/spatial-clustering/include/fishnet/IDReduceFunction.hpp
+++ lib/workflow/spatial-clustering/include/fishnet/IDReduceFunction.hpp
@@ -3,6 +3,7 @@
 #include <fishnet/Polygon.hpp>
 #include "SettlementShape.hpp"
 #include <fishnet/Task.hpp>
+#include <fishnet/OGRFieldAdapter.hpp>
 
 /**
  * @brief Reduce function Functor that merges a set of settlements polygons into a single settlement multi-polygon.
@@ -19,7 +20,7 @@ struct IDReduceFunction{
     template<fishnet::geometry::IPolygon P>
     SettlementShape<fishnet::geometry::MultiPolygon<P>> operator()( const std::vector<SettlementShape<P>> & settlementPolygons) const noexcept {
         size_t id = std::ranges::fold_left(settlementPolygons,0,[](size_t current, const auto & settlementPolygon){return current + settlementPolygon.key();}); 
-        id = normalizeToShpFileIntField(id);
+        id = fishnet::normalizeToShpFileIntField(id);
         fishnet::geometry::MultiPolygon<P> resultGeometry {settlementPolygons | std::views::transform([](const auto & settPoly){return static_cast<P>(settPoly);}),true};
         return SettlementShape<fishnet::geometry::MultiPolygon<P>>(id,outputFileRef,resultGeometry);
     }
