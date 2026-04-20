a09cbc0bce978dfe165eab07d7eb7e5279809b44
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-04-20 14:31:15 +0200

    update to 1.2.0

--- prod/docker/update.sh
+++ prod/docker/update.sh
@@ -3,9 +3,9 @@ PATH_TO_PROJECT_ROOT=../../
 VERSION=$(grep -oP 'project\s*\([^)]*VERSION\s+\K[0-9]+\.[0-9]+\.[0-9]+' $PATH_TO_PROJECT_ROOT/CMakeLists.txt)
 IMAGE=fishnet-apps
 docker build -t logru/$IMAGE:$VERSION -t logru/$IMAGE:latest -f Dockerfile.apps $PATH_TO_PROJECT_ROOT
-# docker login 
-# docker push logru/$IMAGE:$VERSION 
-# docker push logru/$IMAGE:latest
+docker login 
+docker push logru/$IMAGE:$VERSION 
+docker push logru/$IMAGE:latest
 
 # Update fishnet-deps
 # IMAGE=fishnet-deps
